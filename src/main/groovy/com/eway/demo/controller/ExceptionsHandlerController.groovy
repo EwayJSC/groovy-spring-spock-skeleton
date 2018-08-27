06494e6052123e928e1375c0bdac3904c81f7938
Tu Pham <phamptu@gmail.com>
2018-08-27 21:30:29 +0700

    drop auth exception

--- src/main/groovy/com/eway/demo/controller/ExceptionsHandlerController.groovy
+++ src/main/groovy/com/eway/demo/controller/ExceptionsHandlerController.groovy
@@ -2,7 +2,6 @@ package com.eway.demo.controller
 
 import com.eway.common.exception.EwayException
 import com.eway.common.SystemError
-import org.apache.shiro.authc.AuthenticationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -32,14 +31,4 @@ class ExceptionsHandlerController extends ResponseEntityExceptionHandler {
             .fromPath(request.getContextPath())
             .withMessage("Unknown exception").build(), HttpStatus.INTERNAL_SERVER_ERROR)
     }
-
-    @ExceptionHandler(AuthenticationException.class)
-    final ResponseEntity<SystemError> AuthenticationException(AuthenticationException ex, WebRequest request) {
-        return new ResponseEntity<SystemError>(new SystemError.Builder()
-            .details(ex.getMessage())
-            .fromPath(request.getContextPath())
-            .withMessage("User UnAuthentication").build(),
-            HttpStatus.UNAUTHORIZED)
-    }
-
 }
